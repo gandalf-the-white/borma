02ab521c4aab6c38d576b9d9b44f18accf8834f7
gandalf the white <gandalf_the_white@orange.fr>
2026-01-26 12:22:50 +0100

    Update

--- platform/main.tf
+++ platform/main.tf
@@ -2,27 +2,27 @@
 ## C L U S T E R S
 ####################################################################################
 
-# module "south" {
-#   source      = "./modules/kube"
-#   area        = "south"
-#   masters     = var.masters
-#   workers     = var.workers
-#   nbmasters   = 1
-#   nbworkers   = 1
-#   prefix      = "192.188.200" # VLAN 200
-#   bridge      = var.bridge
-#   vlan        = 200
-#   nameserver  = var.nameserver
-#   target_node = var.target_node
-#   clone       = "ubuntu-2404-tmpl"
-#   size        = 30
-#   storage     = var.storage
-#   cloudinit   = var.cloudinit
-#   proxy       = var.proxy
-#   userctn     = var.userctn
-#   publkeyctn  = var.publkeyctn
-#   privkeyctn  = var.privkeyctn
-# }
+module "south" {
+  source      = "./modules/kube"
+  area        = "south"
+  masters     = var.masters
+  workers     = var.workers
+  nbmasters   = 1
+  nbworkers   = 1
+  prefix      = "192.188.200" # VLAN 200
+  bridge      = var.bridge
+  vlan        = 200
+  nameserver  = var.nameserver
+  target_node = var.target_node
+  clone       = "ubuntu-2404-tmpl"
+  size        = 30
+  storage     = var.storage
+  cloudinit   = var.cloudinit
+  proxy       = var.proxy
+  userctn     = var.userctn
+  publkeyctn  = var.publkeyctn
+  privkeyctn  = var.privkeyctn
+}
 
 ####################################################################################
 ## H L S
@@ -52,10 +52,10 @@ module "hls" {
 ## O U T P U T
 ####################################################################################
 
-# output "master_south_ip_address" {
-#   description = "South Masters IP Address"
-#   value       = module.south
-# }
+output "master_south_ip_address" {
+  description = "South Masters IP Address"
+  value       = module.south
+}
 
 output "hls_server_ip_address" {
   description = "Hls Server IP Address"
