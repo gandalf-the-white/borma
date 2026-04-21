b3ff958bdfecb09674ba4d246f85d57f7d5b99e8
gandalf the white <gandalf_the_white@orange.fr>
2026-04-21 15:54:03 +0200

    Update

--- platform/main.tf
+++ platform/main.tf
@@ -90,7 +90,7 @@ module "lisp" {
   vlan        = 200
   nameserver  = var.nameserver
   target_node = var.target_node
-  clone       = "ubuntu-2404-tmpl"
+  clone       = "debian-13-tmpl"
   size        = 30
   storage     = var.storage
   cloudinit   = var.cloudinit
